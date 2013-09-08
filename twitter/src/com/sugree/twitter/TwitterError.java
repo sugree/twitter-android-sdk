af51a370b38f19bb2cbf90b457754f9f866cf29d
Baptiste Robert <baptistex.robert@intel.com>
2013-09-08 18:49:15 +0200

    Clean-up TwitterError class
    The TwitterError class don't respect the Android coding style.
    
    This patch convert indents to spaces, remove useless lines
    in TwitterError class.
    
    This class is identical to DialogError, so one of them is useless.
    As the other class, Javadoc is missing.
    
    Signed-off-by: Baptiste Robert <baptistex.robert@intel.com>

--- twitter/src/com/sugree/twitter/TwitterError.java
+++ twitter/src/com/sugree/twitter/TwitterError.java
@@ -1,28 +1,26 @@
 package com.sugree.twitter;
 
 public class TwitterError extends Throwable {
+    private static final long serialVersionUID = 6626439442641443626L;
 
-	private static final long serialVersionUID = 6626439442641443626L;
+    private static int mErrorCode = 0;
+    private static String mErrorType;
 
-	private int mErrorCode = 0;
-	private String mErrorType;
-	
-	public TwitterError(String message) {
-		super(message);
-	}
+    public TwitterError(String message) {
+        super(message);
+    }
 
-	public TwitterError(String message, String errorType, int errorCode) {
-		super(message);
-		mErrorType = errorType;
-		mErrorCode = errorCode;
-	}
+    public TwitterError(String message, String errorType, int errorCode) {
+        super(message);
+        mErrorType = errorType;
+        mErrorCode = errorCode;
+    }
 
-	public int getErrorCode() {
-		return mErrorCode;
-	}
+    public int getErrorCode() {
+        return mErrorCode;
+    }
 
-	public String getErrorType() {
-		return mErrorType;
-	}
-
+    public String getErrorType() {
+        return mErrorType;
+    }
 }
